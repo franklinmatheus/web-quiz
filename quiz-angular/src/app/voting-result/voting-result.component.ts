b6cc7686cb37926d9c4765541324fbad37aff715
franklinmatheus <franklinmatheus1@gmail.com>
2021-07-17 02:00:03 -0300

    [angular] % in voting-result

--- quiz-angular/src/app/voting-result/voting-result.component.ts
+++ quiz-angular/src/app/voting-result/voting-result.component.ts
@@ -8,4 +8,9 @@ import { Component, Input } from '@angular/core';
 export class VotingResultComponent {
   @Input() votes: []
   constructor() { }
+
+  perc(index: number) {
+    let total = this.votes.reduce((sum, item) => {return sum + item['count']}, 0)
+    return (this.votes[index]['count']/total*100).toFixed(2)       
+  }
 }
