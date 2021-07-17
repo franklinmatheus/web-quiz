a34f3286549062fc465dd809acae6127a1aea4a9
franklinmatheus <franklinmatheus1@gmail.com>
2021-07-17 18:44:30 -0300

    [svelte] % in result

--- quiz-svelte/src/VotingResult.svelte
+++ quiz-svelte/src/VotingResult.svelte
@@ -1,12 +1,17 @@
 <script>
     export let votes
+
+    function perc(index) {
+        let total = votes.reduce((sum, item) => {return sum + item.count}, 0)
+        return (votes[index].count/total*100).toFixed(2)
+    }
 </script>
 
 <div>
     <ol>
         {#each votes as item, index (item)}
             <li>
-                {item.option}: {item.count} votes
+                {item.option}: {item.count} votes ({perc(index)}%)
             </li>
         {/each}
     </ol>
