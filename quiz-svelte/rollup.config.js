5890cdef1d83c9000854b18531e01d54508a8bbc
franklinmatheus <franklinmatheus1@gmail.com>
2021-07-17 18:52:18 -0300

    [svelte] chromatic + cleaning up wd

--- quiz-svelte/rollup.config.js
+++ quiz-svelte/rollup.config.js
@@ -4,7 +4,6 @@ import resolve from "@rollup/plugin-node-resolve";
 import livereload from "rollup-plugin-livereload";
 import { terser } from "rollup-plugin-terser";
 import css from "rollup-plugin-css-only";
-import copy from "rollup-plugin-copy-assets";
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -62,9 +61,6 @@ export default {
       dedupe: ["svelte"],
     }),
     commonjs(),
-    copy({
-      assets: ["src/assets"],
-    }),
 
     // In dev mode, call `npm run start` once
     // the bundle has been generated
